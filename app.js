9f6f29f5df362599f27dd1917d9654966626c569
ahsansamad <ingeniouslogo@gmail.com>
2024-05-30 02:41:07 +0500

    completed on my way

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
-
-
-
-function keyLong(event){
-    var count = 1
-    //  console.log(event)
-    if(event.keyCode >= "65" && event.keyCode <=  "90"){
-        console.log("upper")
-        var length = document.getElementById('a') 
-        for(i = 0 ; i < length.value.length ; i++){
-
-            count++
-        }
-        document.getElementById('display').innerHTML = `you press ${event.key} ${count} times`
-    
+function keyLong(event) {
+  var count = 1;
+  if (event.keyCode) {
+    var length = document.getElementById("a");
+    for (i = 0; i < length.value.length; i++) {
+      count++;
+    }
+    document.getElementById(
+      "display"
+    ).innerHTML = `You Pressed <b>(${event.key})</b> ${count} times`;
+  }
 }
-// console.log(event)
+function abc(event) {
+  document.getElementById("a").value = "";
 }
 
-document.getElementById('a').addEventListener( "keydown" ,function(){
-    keyLong(event)
-})
-
-
-
-
-    
+document.getElementById("a").addEventListener("keydown", function () {
+  keyLong(event);
+});
