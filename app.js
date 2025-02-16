6f22c1db13cc43fcc1d1c531293ff24b2478db10
ahsansamad <ingeniouslogo@gmail.com>
2025-02-16 15:40:39 +0500

    commit

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-function keyLong(event) {
-  var count = 1;
-  if (event.keyCode) {
-    var length = document.getElementById("a");
-    for (i = 0; i < length.value.length; i++) {
-      count++;
-    }
-    document.getElementById(
-      "display"
-    ).innerHTML = `You Pressed <b>(${event.key})</b> ${count} times`;
-  }
-}
-function abc(event) {
-  document.getElementById("a").value = "";
-}
+// function keyLong(event) {
+//   var count = 1;
+//   if (event.keyCode) {
+//     var length = document.getElementById("a");
+//     for (i = 0; i < length.value.length; i++) {
+//       count++;
+//     }
+//     document.getElementById(
+//       "display"
+//     ).innerHTML = `You Pressed <b>(${event.key})</b> ${count} times`;
+//   }
+// }
+// function abc(event) {
+//   document.getElementById("a").value = "";
+// }
 
-document.getElementById("a").addEventListener("keydown", function () {
-  keyLong(event);
-});
+// document.getElementById("a").addEventListener("keydown", function () {
+//   keyLong(event);
+// });
+// ========================
+// ========================
+
